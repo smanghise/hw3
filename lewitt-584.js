4d9a84c011452c269256c7d57bb580ffeedd86b2
smanghise <35621921+smanghise@users.noreply.github.com>
2018-02-03 13:00:48 -0800

    Create lewitt-584.js

---
+++ lewitt-584.js
@@ -0,0 +1,21 @@
+colorMode(HSB);
+background(230, 204, 0);
+
+rect(10, 10, width-20, height-20);
+
+//purple
+fill(270, 150, 100);
+rect(15, 15, width/2, height/2);
+
+//black
+fill(130, 60, 0);
+rect(15, 175, width/2-10, height/2-20);
+
+//green
+fill(130, 150, 100);
+rect(175, 15, width/2-20, height/2-10);
+
+//orange
+fill(30, 150, 100);
+rect(175, 175, width/2-20, height/2-20);
+
