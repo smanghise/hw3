d61c9cc1a1947ce287807c1d978fc76e4ee16d3c
smanghise <35621921+smanghise@users.noreply.github.com>
2018-02-03 13:35:27 -0800

    Update drawing-new-colors.js

--- drawing-new-colors.js
+++ drawing-new-colors.js
@@ -1,1 +1,28 @@
+function setup() { 
+  createCanvas(400, 400);
+} 
 
+var diameter = 10;
+
+function draw() { 
+  if (mouseIsPressed) {
+    ellipse(mouseX, mouseY, diameter);
+  }
+}
+
+function keyPressed() {
+  print(key);
+  if (key == 'R') {
+    fill(255,102, 102);
+  } else if (key == 'G') {
+    fill(178, 255, 102);
+  } else if (key == 'B') {
+    fill(153, 204, 255); 
+  } else if (key == 'O') {
+    fill(255, 153, 51); 
+  } else if (key == 'P') {
+    fill(255, 102, 178); 
+  } else if (key == 'Y') {
+    fill(255, 255, 0); 
+  }
+}
